73e2d2ce7f068ecc54eebd0cfcf052fca4700b8c
José Roland <jose.sroland@gmail.com>
2025-06-16 21:17:11 -0400

    Conversão com One-Hot Encoding Conversão de variáveis para booleanas Conversão de gender, Contract, PaymentoMethod e InternetService para múltiplas colunas de OHE

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import pandas as pd
 
+columnsToNumeric = ["SeniorCitizen",
+                    "Partner",
+                    "Dependents",
+                    "PhoneService",
+                    "MultipleLines",
+                    "OnlineSecurity",
+                    "OnlineBackup",
+                    "DeviceProtection",
+                    "TechSupport",
+                    "StreamingTV",
+                    "StreamingMovies",
+                    "PaperlessBilling",
+                    "TotalCharges",
+                    "Churn"] # Lista das colunas que terão seus tipos convertidos de object para numeric
+
+categoricalColumns = ["gender", "Contract", "PaymentMethod"]
+# Lista das colunas que vão passar pelo one-hot encoding
+
 pd.set_option('future.no_silent_downcasting', True)
 # Evita warning de que função replace mude dados de forma automática (downcasting)
 
@@ -15,13 +33,12 @@ for column in dataframe.columns:
         # Remove espaços em branco dos valores, cajo haja algum
 
         dataframe[column] = dataframe[column].replace({
-            "No internet service": "Yes",
-            "No phone service": "No",
-            "0": "No",
-            "1": "Yes",
-            0: "No",
-            1: "Yes"
-        }) # Troca valores inconsistentes por Yes ou No
+            "No phone service": 0,
+            "No": 0,
+            "Yes": 1,
+            "0": 0,
+            "1": 1
+        }) # Troca valores por 0 ou 1
 
 dataframe.loc[dataframe["TotalCharges"].isnull(), "TotalCharges"] = 0
 # Preenche as linhas de TotalCharges nulas com 0
